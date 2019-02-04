8b40b5762384e73f48b062e4b71e00835dec55f5
Mathias Ronimus <mathiar225@gmail.com>
2019-02-05 11:19:57 +1300

    Add infrastructure for deleting content and hDividers

--- creator/src/app/content-pane/content-pane.component.ts
+++ creator/src/app/content-pane/content-pane.component.ts
@@ -13,8 +13,12 @@ export class ContentPaneComponent implements OnInit, AfterViewInit {
 
   addingTerm: boolean;
   lastTermAddText = '';
-  terms: TermSelector[];
+  terms: string[];
   @ViewChildren('termInput') termInputEl: QueryList<any>;
+
+  hDividers = 0;
+
+  selectedRef = '';
 
   constructor(private undoRedo: UndoRedoService) {
     this.containers = [
@@ -85,11 +89,60 @@ export class ContentPaneComponent implements OnInit, AfterViewInit {
   }
 
   /**
+   * Get a string array for each hDivider. Used by the template.
+   */
+  getHDividerArray(): string[] {
+    const toReturn: string[] = [];
+    for (let i = 0; i < this.hDividers; i++) {
+      toReturn.push('' + i);
+    }
+    return toReturn;
+  }
+
+  /**
+   * Add a new hDivider.
+   */
+  addHDivider() {
+    const newState: any = this.undoRedo.getStateClone();
+    if (!newState.hDividers) {
+      newState.hDividers = 0;
+    }
+    newState.hDividers++;
+    this.undoRedo.publishChange(newState);
+  }
+
+  /**
+   * Select a container or some content.
+   * @param ref The reference of the content.
+   *            Here a 'c' prefix is used for containers, which isn't valid in the context of a canvas controller.
+   * @param e The mouse event.
+   */
+  select(ref: string, e: MouseEvent) {
+    this.selectedRef = ref;
+    e.stopPropagation();
+  }
+
+  /**
+   * Deselect whatever is selected.
+   */
+  deselect() {
+    this.selectedRef = '';
+  }
+
+  /**
+   * Delete the currently selected content.
+   */
+  delete() {
+    console.log(this.selectedRef);
+  }
+
+  /**
    * Update the view when the state changes.
    * @param newState The new state.
    */
   updateState(newState: any) {
     this.terms = newState.terms ? newState.terms : [];
+    this.hDividers = newState.hDividers ? newState.hDividers : 0;
   }
 }
 
